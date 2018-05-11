d0026bd9ba6ed866689024312c415067424255c7
Tom Sherborne <tr.sherborne@gmail.com>
2018-05-11 21:32:37 +0100

    Remove redundant fix

--- src/model.py
+++ src/model.py
@@ -115,8 +115,7 @@ class CaptioningModel(object):
         self.input_seqs = batch['input_seqs']
         self.target_seqs = batch['target_seqs']
         self.input_mask = tf.cast(batch['input_mask'], dtype=tf.float32)
-        # self.input_seqs_len = tf.squeeze(batch['seqs_len'])
-        self.input_seqs_len = [3]
+        self.input_seqs_len = tf.squeeze(batch['seqs_len'])
         
         # Get reference captions if testing
         self.reference_captions = batch['complete_seqs']
